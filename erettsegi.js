a270cd395560ebfadb140cc8bd1cf2c5470cf2c2
torokmarkwerner <98919050+torokmarkwerner@users.noreply.github.com>
2023-09-21 18:17:00 +0200

    -

---
+++ erettsegi.js
@@ -0,0 +1,174 @@
+/*
+type = "uoe-word-transformation"
+type = "uoe-closed-gap-filling"
+type = "uoe-free-gap-filling"
+type = "reading-dialogue"
+type = "reading-text"
+type = "reading-matching"
+type = "reading-true-or-false"
+*/
+
+type = "reading-dialogue"
+
+score = 0
+
+solutions = [
+"Is there a membership fee?",
+"What can I do if my library card is not accepted?",
+"How many books can I borrow?",
+"Can someone else pick up my books?",
+"How long can I keep my borrowed materials?",
+"How many times can I renew the items that I borrowed?",
+"What happens if I lose or damage the borrowed item?",
+"What can I take into the reading rooms?",
+"If the library doesn’t have a title I’m looking for, is it possible to order it from another library?"
+]
+
+picked = ""
+function pickAnswer(el) {
+selection = document.querySelector("#word-list > .picked");
+if(selection!=null) {
+selection.classList.toggle("picked")
+}
+picked = el.innerHTML
+el.classList.toggle("picked")
+}
+
+function dropAnswer(el) {
+isVisibleThen()
+if (type == "reading-text") {
+el.style.display = "inline"
+}
+el.innerHTML = picked
+picked = ""
+selection = document.querySelector("#word-list > .picked");
+if(selection!=null) {
+selection.outerHTML = ""
+}
+}
+
+if (type == "reading-matching") {
+taskGaps = document.querySelectorAll("#task > div > .gap")
+} else if (type == "reading-true-or-false") {
+taskGaps = document.querySelectorAll(".radio-container:not(.default)")
+} else /*if (type == "uoe-word-transformation" || type == "uoe-closed-gap-filling" || type == "uoe-free-gap-filling" || type == "reading-dialogue" || type == "reading-text") */ {
+taskGaps = document.querySelectorAll("#task > .gap")
+}
+
+wordListGaps = document.querySelectorAll("#word-list > .gap")
+
+/*taskGaps are: GAPS if it's a gap-filling task, RADIO-CONTAINERS if it's a true-or-false task, and GAPS inside a div tag if it's a matching task.*/
+
+if (document.getElementById("word-list")) {
+for (i=0; i<taskGaps.length; i++) {
+taskGaps[i].addEventListener("click",function(){if(this.innerText == ""){dropAnswer(this);}else{
+wordList = document.getElementById("word-list"); gap = document.createElement("DIV"); gap.innerHTML = this.innerHTML; gap.classList.add("gap"); gap.addEventListener("click",function(){pickAnswer(this)}); wordList.appendChild(gap); this.innerHTML = ""; if (type == "reading-text") {this.style.display = "inline-block"}
+}});
+}
+}
+
+for (i=0; i<wordListGaps.length; i++) {
+wordListGaps[i].addEventListener("click",function(){pickAnswer(this);});
+}
+
+/*If there are options (word-list), word-list gaps can be picked and dropped into task gaps. OR if a taskGap has already been filled in, the gap goes back to the word-list.*/
+
+function checkAnswers() {
+for (i=0; i<taskGaps.length; i++) {
+
+if (type == "uoe-word-transformation" || type == "uoe-free-gap-filling") {
+taskGaps[i].classList.remove("incorrect");
+
+if (!(taskGaps[i].disabled) && taskGaps[i].value.toLowerCase() != "" && (taskGaps[i].value.toLowerCase() == solutions[i] || solutions[i].includes(taskGaps[i].value.toLowerCase()) == true)) {
+taskGaps[i].classList.add("correct")
+taskGaps[i].disabled = true
+score++;
+} else if (!(taskGaps[i].disabled)) {
+taskGaps[i].classList.add("incorrect")
+}
+
+} else if (type == "uoe-closed-gap-filling" || type == "reading-dialogue" || type == "reading-text" || type == "reading-matching") {
+taskGaps[i].classList.remove("incorrect");
+if (!(taskGaps[i].classList.contains("correct")) && taskGaps[i].innerHTML == solutions[i] && !(taskGaps[i].classList.contains("gap-disabled"))) {
+taskGaps[i].classList.add("correct");
+taskGaps[i].replaceWith(taskGaps[i].cloneNode(true));
+score++;
+} else {
+taskGaps[i].classList.add("incorrect")
+}
+
+} else {
+/*TRUE-OR-FALSE*/
+taskGaps[i].querySelectorAll('label > [type="radio"]').forEach((element) => {element.nextElementSibling.classList.remove("incorrect")})
+
+radio = taskGaps[i].querySelector(":checked");
+
+if (radio != null) {
+if (radio.nextElementSibling.innerHTML == solutions[i] && !(radio.disabled)) {
+taskGaps[i].querySelectorAll('label > [type="radio"]').forEach((element) => {element.disabled = true;});
+radio.nextElementSibling.classList.add("correct");
+radio.checked = false;
+score++
+} else if (!(radio.disabled)) {
+radio.nextElementSibling.classList.add("incorrect")
+radio.checked = false
+}
+}
+
+}
+}
+alert(score + "/" + (taskGaps.length))
+}
+
+function showAnswers(){
+for (i=0; i<taskGaps.length; i++) {
+if (type == "uoe-word-transformation" || type == "uoe-free-gap-filling") {
+taskGaps[i].value = solutions[i].toString().replaceAll(","," / ");
+taskGaps[i].disabled = true;
+taskGaps[i].classList.add("gap-disabled");
+taskGaps[i].classList.remove("gap","incorrect");
+taskGaps[i].style.width = taskGaps[i].value.length + "ch";
+taskGaps[i].replaceWith(taskGaps[i].cloneNode(true));
+
+} else if (type == "uoe-closed-gap-filling" || type == "reading-dialogue" || type == "reading-text" || type == "reading-matching") {
+taskGaps[i].innerHTML = solutions[i]
+taskGaps[i].classList.add("gap-disabled");
+taskGaps[i].classList.remove("gap","incorrect");
+taskGaps[i].replaceWith(taskGaps[i].cloneNode(true));
+
+} else {
+/*TRUE-OR-FALSE*/
+for (k=0; k<taskGaps[i].querySelectorAll('label > [type="radio"]').length; k++) {
+radio = taskGaps[i].querySelectorAll('label > [type="radio"]')[k];
+radio.nextElementSibling.classList.remove("incorrect","correct");
+radio.disabled = true
+if (radio.nextElementSibling.innerHTML != solutions[i]) {
+radio.checked = false;
+} else {
+radio.checked = true;
+}
+}
+
+}
+}
+}
+
+function isVisibleThen(){
+for (i=0; i<wordListGaps.length; i++) {
+    if (wordListGaps[i].parentElement && (((wordListGaps[i].offsetTop + wordListGaps[i].offsetHeight) > (wordListGaps[i].parentElement.scrollTop + wordListGaps[i].parentElement.offsetHeight)) || wordListGaps[i].offsetTop < wordListGaps[i].parentElement.scrollTop)) {
+    wordListGaps[i].style.opacity = "0"
+    wordListGaps[i].style.visibility = "hidden"
+    } else {
+    wordListGaps[i].style.visibility = "visible"
+    wordListGaps[i].style.opacity = "1"
+    }
+}
+}
+
+isVisibleThen()
+
+if(document.getElementById("word-list")) {
+document.getElementById("word-list").addEventListener("scroll",function(){isVisibleThen()
+/*alert(wordListGaps[4].offsetTop + " " + " ")*/
+})
+}
