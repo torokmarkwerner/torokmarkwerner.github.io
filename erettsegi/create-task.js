92d32487785ee580012a1308bec1e6e33d80385a
torokmarkwerner <torokmarkwerner@gmail.com>
2023-10-01 01:26:06 +0200

    uoe-multiple-choice

--- erettsegi/create-task.js
+++ erettsegi/create-task.js
@@ -1,143 +1,166 @@
 fetch("/erettsegi/data.json").then((response) => {
-  return response.json()
+    return response.json()
 }).then((data) => {
-  populateHTML(data)
+    populateHTML(data)
 })
 
 
 function populateHTML(x) {
-  taskJSON = x.find(y => y.taskId == taskId);
-  type = taskJSON.type;
-  solutions = JSON.parse(taskJSON.solutions);
-  taskTitle = taskJSON.taskTitle;
-  instructions = JSON.parse(taskJSON.instructions);
-  wordList = JSON.parse(taskJSON.wordList);
-  textTitle = taskJSON.textTitle;
-  example = taskJSON.example;
-  text = taskJSON.text;
-  text2 = taskJSON.text2;
+    taskJSON = x.find(y => y.taskId == taskId);
+    type = taskJSON.type;
+    solutions = JSON.parse(taskJSON.solutions);
+    taskTitle = taskJSON.taskTitle;
+    instructions = JSON.parse(taskJSON.instructions);
+    wordList = JSON.parse(taskJSON.wordList);
+    textTitle = taskJSON.textTitle;
+    example = taskJSON.example;
+    text = taskJSON.text;
+    text2 = taskJSON.text2;
 
-  taskBody = document.createElement("DIV");
-  taskBody.className = type;
-  h2 = document.createElement("H2");
-  h2.innerHTML = taskTitle;
-  taskBody.appendChild(h2)
+    taskBody = document.createElement("DIV");
+    taskBody.className = type;
+    h2 = document.createElement("H2");
+    h2.innerHTML = taskTitle;
+    taskBody.appendChild(h2)
 
-  ul = document.createElement("UL");
+    ul = document.createElement("UL");
 
-  for (i = 0; i < instructions.length; i++) {
-    li = document.createElement("LI");
-    li.innerHTML = instructions[i]
-    ul.appendChild(li)
-  }
-
-  taskBody.appendChild(ul)
-
-  if (wordList.length > 0) {
-    wordListContainer = document.createElement("DIV");
-    wordListContainer.id = "word-list"
-
-    for (i = 0; i < wordList.length; i++) {
-      div = document.createElement("DIV");
-      div.innerHTML = Object.keys(wordList[i])[0]
-      div.id = taskId + "-" + Object.values(wordList[i])[0]
-      div.classList.add("gap")
-      wordListContainer.appendChild(div)
+    for (i = 0; i < instructions.length; i++) {
+        li = document.createElement("LI");
+        li.innerHTML = instructions[i]
+        ul.appendChild(li)
     }
 
-    /* taskBody.appendChild(wordListContainer) */
+    taskBody.appendChild(ul)
 
-  }
+    if (wordList.length > 0 && type != "uoe-multiple-choice") {
+        wordListContainer = document.createElement("DIV");
+        wordListContainer.id = "word-list"
 
-  task = document.createElement("DIV");
-  task.id = "task";
+        for (i = 0; i < wordList.length; i++) {
+            div = document.createElement("DIV");
+            div.innerHTML = Object.keys(wordList[i])[0]
+            div.id = taskId + "-" + Object.values(wordList[i])[0]
+            div.classList.add("gap")
+            wordListContainer.appendChild(div)
+        }
 
-  h3 = document.createElement("H3");
-  h3.innerHTML = textTitle;
+        /* taskBody.appendChild(wordListContainer) */
 
-  /* task.appendChild(h3) */
-
-  if (type == "uoe-closed-gap-filling" || type == "reading-text") {
-    task.innerHTML = text.replace(/\(\d+\)/, "<span class='default-gap'>" + example + "</span> ").replace(/\(\d+\)/g, "<span class='gap'></span>").replace(/\(/g, "<b>(").replace(/\)/g, ")</b>")
-  } else if (type == "uoe-word-transformation" || type == "uoe-free-gap-filling") {
-    task.innerHTML = text.replace(/\(\d+\)/, "<span class='default-gap'>" + example + "</span> ").replace(/\(\d+\)/g, "<input class='gap'> ")
-  } else if (type == "reading-dialogue") {
-    task.innerHTML = text.replace(/\(\d+\)/, "<div class='default-gap'>" + example + "</div> ").replace(/\(\d+\)/g, "<div class='gap'></div>")
-  } else if (type == "reading-matching") {
-    task.innerHTML = text2.replace("<div class='gap'></div>", "<div class='default-gap'>" + example + "</div> ");
-  } else {
-    /*type == "reading-true-or-false"*/
-    task.innerHTML = text2;
-    tfs = task.querySelectorAll(".radio-container")[0].querySelectorAll("label input");
-    for (i = 0; i < tfs.length; i++) {
-      tfs[i].disabled = true
-      if (tfs[i].nextElementSibling.innerText == example) {
-        tfs[i].checked = true
-      }
-    }
-  }
-
-  /* task.appendChild(div) */
-
-  if (["reading-matching", "reading-true-or-false"].includes(type)) {
-
-    taskBody.appendChild(h3)
-
-    taskText = document.createElement("DIV");
-    taskText.innerHTML = text;
-    taskBody.appendChild(taskText);
-
-    if (wordList.length > 0) {
-      taskBody.appendChild(wordListContainer)
     }
 
-    taskBody.appendChild(task)
+    task = document.createElement("DIV");
+    task.id = "task";
 
-  } else {
-    if (wordList.length > 0) {
-      taskBody.appendChild(wordListContainer)
+    h3 = document.createElement("H3");
+    h3.innerHTML = textTitle;
+
+    /* task.appendChild(h3) */
+
+    if (type == "uoe-closed-gap-filling" || type == "reading-text") {
+        task.innerHTML = text.replace(/\(\d+\)/, "<span class='default-gap'>" + example + "</span> ").replace(/\(\d+\)/g, "<span class='gap'></span>").replace(/\(/g, "<b>(").replace(/\)/g, ")</b>")
+    } else if (type == "uoe-word-transformation" || type == "uoe-free-gap-filling") {
+        task.innerHTML = text.replace(/\(\d+\)/, "<span class='default-gap'>" + example + "</span> ").replace(/\(\d+\)/g, "<input class='gap'> ")
+    } else if (type == "reading-dialogue") {
+        task.innerHTML = text.replace(/\(\d+\)/, "<div class='default-gap'>" + example + "</div> ").replace(/\(\d+\)/g, "<div class='gap'></div>")
+    } else if (type == "reading-matching") {
+        task.innerHTML = text2.replace("<div class='gap'></div>", "<div class='default-gap'>" + example + "</div> ");
+    } else if (type == "uoe-multiple-choice") {
+        task.innerHTML = text.replace(/\(\d+\)/, "<select class='default-gap'></select>").replace(/\(\d+\)/g, "<select class='gap'></select>")
+
+        for (i = 0; i < task.querySelectorAll("select").length; i++) {
+            select = task.querySelectorAll("select")[i]
+            optionDefault = document.createElement("OPTION");
+            optionDefault.style.display = "none"
+            select.appendChild(optionDefault)
+            for (z = 0; z < Object.keys(wordList[i]).length; z++) {
+                option = document.createElement("OPTION");
+                option.value = (Object.keys(wordList[i]))[z]
+                option.innerHTML = (Object.values(wordList[i]))[z]
+                select.appendChild(option)
+            }
+            if (i == 0) {
+                select.querySelector("[value='" + example + "']").selected = true
+                select.querySelectorAll(":not([value='" + example + "'])").forEach(x => x.disabled = true)
+            }
+        }
+
+
+
+
+    } else {
+        /*type == "reading-true-or-false"*/
+        task.innerHTML = text2;
+        tfs = task.querySelectorAll(".radio-container")[0].querySelectorAll("label input");
+        for (i = 0; i < tfs.length; i++) {
+            tfs[i].disabled = true
+            if (tfs[i].nextElementSibling.innerText == example) {
+                tfs[i].checked = true
+            }
+        }
     }
-    task.insertBefore(h3, task.firstChild)
-    taskBody.appendChild(task)
-  }
+
+    /* task.appendChild(div) */
+
+    if (["reading-matching", "reading-true-or-false"].includes(type)) {
+
+        taskBody.appendChild(h3)
+
+        taskText = document.createElement("DIV");
+        taskText.innerHTML = text;
+        taskBody.appendChild(taskText);
+
+        if (wordList.length > 0) {
+            taskBody.appendChild(wordListContainer)
+        }
+
+        taskBody.appendChild(task)
+
+    } else {
+        if (wordList.length > 0 && type != "uoe-multiple-choice") {
+            taskBody.appendChild(wordListContainer)
+        }
+        task.insertBefore(h3, task.firstChild)
+        taskBody.appendChild(task)
+    }
 
 
-  script = document.createElement("SCRIPT");
-  script.src = "https://torokmarkwerner.github.io/erettsegi/statements-pick-and-drop.js"
-  taskBody.appendChild(script)
+    script = document.createElement("SCRIPT");
+    script.src = "/erettsegi/statements-pick-and-drop.js"
+    taskBody.appendChild(script)
 
-  checkButton = document.createElement("BUTTON");
-  checkButton.className = "check-button";
-  checkButton.innerText = "CHECK ANSWERS";
-  checkButton.addEventListener("click", checkAnswers)
+    checkButton = document.createElement("BUTTON");
+    checkButton.className = "check-button";
+    checkButton.innerText = "CHECK ANSWERS";
+    checkButton.addEventListener("click", checkAnswers)
 
-  showButton = document.createElement("BUTTON")
-  showButton.className = "show-button";
-  showButton.innerText = "SHOW ANSWERS";
-  showButton.addEventListener("click", showAnswers)
+    showButton = document.createElement("BUTTON")
+    showButton.className = "show-button";
+    showButton.innerText = "SHOW ANSWERS";
+    showButton.addEventListener("click", showAnswers)
 
-  taskBody.appendChild(checkButton);
-  taskBody.appendChild(showButton);
+    taskBody.appendChild(checkButton);
+    taskBody.appendChild(showButton);
 
-modalClose = document.createElement("SPAN");
-modalClose.className = "modal-close";
-modalClose.innerHTML = "&times;";
-modalClose.addEventListener("click",function() {this.parentElement.parentElement.style.display = "none"})
+    modalClose = document.createElement("SPAN");
+    modalClose.className = "modal-close";
+    modalClose.innerHTML = "&times;";
+    modalClose.addEventListener("click", function() { this.parentElement.parentElement.style.display = "none" })
 
-modalP = document.createElement("P");
+    modalP = document.createElement("P");
 
-modalBox = document.createElement("DIV");
-modalBox.id = "score-modal";
-modalBox.className = "modal-box";
+    modalBox = document.createElement("DIV");
+    modalBox.id = "score-modal";
+    modalBox.className = "modal-box";
 
-modalBox.appendChild(modalClose);
-modalBox.appendChild(modalP);
+    modalBox.appendChild(modalClose);
+    modalBox.appendChild(modalP);
 
-modalBasis = document.createElement("DIV");
-modalBasis.className = "modal-basis";
+    modalBasis = document.createElement("DIV");
+    modalBasis.className = "modal-basis";
 
-modalBasis.appendChild(modalBox);
+    modalBasis.appendChild(modalBox);
 
-document.getElementById("content-container").appendChild(modalBasis);
-  document.getElementById("content-container").appendChild(taskBody);
+    document.getElementById("content-container").appendChild(modalBasis);
+    document.getElementById("content-container").appendChild(taskBody);
 }
